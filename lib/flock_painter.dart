6a3f92350003f806bf4aaa5a7a86fdfbed6586a2
Joao Novaes <juaum.markux@gmail.com>
2023-04-13 13:15:27 -0300

    implement new boid shape

--- lib/flock_painter.dart
+++ lib/flock_painter.dart
@@ -1,5 +1,8 @@
+import 'dart:math';
+
 import 'package:flocking_simulation/boid.dart';
 import 'package:flutter/material.dart';
+import 'package:vector_math/vector_math.dart' as vector;
 
 class FlockPaint extends CustomPaint {
   @override
@@ -11,39 +14,55 @@ class FlockPaint extends CustomPaint {
 class FlockPainter extends CustomPainter {
   List<Boid> flock;
   FlockPainter(this.flock);
+  static const double boidSize = 8;
+  static const halfSize = boidSize / 2;
+  static const radius = 2.0;
   @override
   void paint(Canvas canvas, Size size) {
-    const radius = 2.0;
-    final List<double> position = [size.width / 2, size.height / 2];
     final paintCenter = Paint()..color = Colors.white30;
-    final paintCircle = Paint()
-      ..color = Colors.white10
-      ..style = PaintingStyle.stroke;
-    canvas.drawCircle(Offset(position[0], position[1]), 1, paintCenter);
+    //final paint = Paint()..color = Colors.white;
+
+    // final paintCircle = Paint()
+    //   ..color = Colors.white10
+    //   ..style = PaintingStyle.stroke;
+
+    // final paintCircleRed = Paint()
+    //   ..color = Colors.red.withOpacity(0.2)
+    //   ..style = PaintingStyle.stroke;
+
+    canvas.drawCircle(Offset(size.width / 2, size.height / 2), 1, paintCenter);
 
     for (var boid in flock) {
       final randompaint = Paint()
-        ..color =
-            Color.fromRGBO(boid.color[0], boid.color[1], boid.color[2], 1);
+        ..color = Color.fromRGBO(boid.color[0], boid.color[1], 255, 1);
+
       if (!boid.position.isNaN) {
-        canvas.drawCircle(
-          Offset(
-            boid.position.x,
-            boid.position.y,
-          ),
-          radius,
-          randompaint,
-        );
-        // canvas.drawCircle(
-        //   Offset(
-        //     boid.position.x,
-        //     boid.position.y,
-        //   ),
-        //   15,
-        //   paintCircle,
-        // );
-      } else {
-        debugPrint('NaN');
+        canvas.save();
+        canvas.translate(boid.position.x, boid.position.y);
+
+        final path = Path()
+          ..moveTo(halfSize + 2, 0)
+          ..lineTo(-halfSize - 1, halfSize - 2)
+          ..lineTo(-halfSize - 1, -halfSize + 2)
+          ..close();
+
+        if (boid.velocity != vector.Vector2.zero() && !boid.velocity.isNaN) {
+          final boidAngle = atan2(boid.velocity.y, boid.velocity.x);
+          canvas.rotate(boidAngle);
+        }
+
+        canvas.drawPath(path, randompaint);
+        canvas.restore();
+
+        //   // perception radius
+        //   canvas.drawCircle(
+        //       Offset(boid.position.x, boid.position.y), 40, paintCircle);
+        //   // separation radius
+        //   canvas.drawCircle(
+        //       Offset(boid.position.x, boid.position.y), 15, paintCircleRed);
+        // } else {
+        //   debugPrint('NaN');
+        // }
       }
     }
   }
